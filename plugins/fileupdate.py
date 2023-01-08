10baffa2b78631c34b577b6487417b8dbc1e400c
izzy-adeeva <88928279+izzy-adeeva@users.noreply.github.com>
2023-01-08 22:38:32 +0700

    Update fileupdate.py

--- plugins/fileupdate.py
+++ plugins/fileupdate.py
@@ -14,7 +14,7 @@ from pyrogram.types import Message
 
 from config import ADMINS, LOGGER
 
-UPSTREAM_REPO = "https://github.com/Onlymeriz/fs"
+UPSTREAM_REPO = "https://github.com/izzy-adeeva/3BUTT"
 
 
 def gen_chlog(repo, diff):
