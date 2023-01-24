7f5bf7b0924639fac986dbd52812884319a48192
xyzsky01 <98657597+xyzsky01@users.noreply.github.com>
2023-01-24 11:25:01 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ class Bot(Client):
         except Exception as a:
             self.LOGGER(__name__).warning(a)
             self.LOGGER(__name__).info(
-                "Bot Berhenti. Gabung Group https://t.me/demusplace untuk Bantuan"
+                "Bot Berhenti. Gabung Group https://t.me/tentangsayaa01 untuk Bantuan"
             )
             sys.exit()
 
@@ -59,7 +59,7 @@ class Bot(Client):
                     f"Pastikan @{self.username} adalah admin di Channel Tersebut, Chat ID F-Subs Channel Saat Ini: {FORCE_SUB_CHANNEL}"
                 )
                 self.LOGGER(__name__).info(
-                    "Bot Berhenti. Gabung Group https://t.me/ramsupportt untuk Bantuan"
+                    "Bot Berhenti. Gabung Group https://t.me/tentangsayaa01 untuk Bantuan"
                 )
                 sys.exit()
 
@@ -79,7 +79,7 @@ class Bot(Client):
                     f"Pastikan @{self.username} adalah admin di Group Tersebut, Chat ID F-Subs Group Saat Ini: {FORCE_SUB_GROUP}"
                 )
                 self.LOGGER(__name__).info(
-                    "Bot Berhenti. Gabung Group https://t.me/ramsupportt untuk Bantuan"
+                    "Bot Berhenti. Gabung Group https://t.me/tentangsayaa01 untuk Bantuan"
                 )
                 sys.exit()
 
@@ -99,7 +99,7 @@ class Bot(Client):
                     f"Pastikan @{self.username} adalah admin di Channel Tersebut, Chat ID F-Subs Channel Saat Ini: {FORCE_SUB_GROUP2}"
                 )
                 self.LOGGER(__name__).info(
-                    "Bot Berhenti. Gabung Group https://t.me/ramsupportt untuk Bantuan"
+                    "Bot Berhenti. Gabung Group https://t.me/tentangsayaa01 untuk Bantuan"
                 )
                 sys.exit()
 
@@ -114,7 +114,7 @@ class Bot(Client):
                 f"Pastikan @{self.username} adalah admin di Channel DataBase anda, CHANNEL_ID Saat Ini: {CHANNEL_ID}"
             )
             self.LOGGER(__name__).info(
-                "Bot Berhenti. Gabung Group https://t.me/ramsupportt untuk Bantuan"
+                "Bot Berhenti. Gabung Group https://t.me/tentangsayaa01 untuk Bantuan"
             )
             sys.exit()
 
